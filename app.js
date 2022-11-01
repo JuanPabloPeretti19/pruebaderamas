6bdf636fd41904179e8766cf88ee03dcc5bc04af
juanpabloperetti19 <juanpabloperetti@gmail.com>
2022-11-01 16:40:21 -0300

    commit nuevo

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const app = express();
 
 app.listen(8000,()=> console.log("servidor corriendo"))
 app.get('/',(req,res)=>{
-   res.send("Ni se te ocurraaa hacer eso sin consultarme?")
+   res.send("Ni se te ocurraaa hacer eso sin consultarme antes?")
 })
