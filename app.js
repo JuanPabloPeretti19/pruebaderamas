269015b40d5d10bace0595a5545d419ae2bb4d55
juanpabloperetti19 <juanpabloperetti@gmail.com>
2022-11-01 14:05:50 -0300

    vv

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const app = express();
 
 app.listen(8000,()=> console.log("servidor corriendo"))
 app.get('/',(req,res)=>{
-   res.send("Hola Mundoo, que te esta ocurriendo?")
+   res.send("Ni se te ocurraaa hacer eso sin consultarme?")
 })
